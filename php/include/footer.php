460c2c63bc0cf19124b6dcc787484ac6daa932cb
Laurent Richir <laurent.richir@icloud.com>
2022-06-13 11:08:46 +0200

    13/06
    ajout css
    infos TP3

--- php/include/footer.php
+++ php/include/footer.php
@@ -38,7 +38,7 @@
 
             <ul id="rs">
                 <li><a href="https://www.facebook.com/LaPtiteLady.wingles" target="_blank">
-                        <i class="fa-brands fa-facebook fa-2x"></i>
+                        <i id="facebook" class="fa-brands fa-facebook fa-2x"></i>
                     </a>
                 </li>
             </ul>
